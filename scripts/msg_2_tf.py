72b3649f8ea44388f258f74e185df9b0a9913908
Shen Li <shentheman.c@gmail.com>
2018-06-07 20:18:14 -0400

    add tf pub

---
+++ scripts/msg_2_tf.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+from __future__ import division
+import rospy, yaml, copy, math, numpy as np, collections, os
+from catkin.find_in_workspaces import find_in_workspaces
+from IPython import embed
+import std_msgs, tf
+import itertools
+import rviz_tools_py.rviz_tools
+import tf.transformations
+
+from phasespace_acquisition.msg import PhaseSpaceMarker, PhaseSpaceMarkerArray
+
+class Msg2Tf(object):
+    def __init__(self):
+        REPO_NAME = "phasespace_acquisition"
+        base_path = find_in_workspaces(
+            search_dirs=['share'],
+            project=REPO_NAME,
+            path='config',
+            first_match_only=True)
+        if len(base_path) == 0:
+            raise Exception("Unable to find base_path.")
+        self.base_path_ = base_path[0]
+
+        self.tracking_config_ = yaml.load(
+            open(self.base_path_ + '/tracking.yaml'))
+
+        self.publish_rate_ = rospy.Rate(
+                self.tracking_config_["publish_frequency"])
+
+        self.br_ = tf.TransformBroadcaster()
+
+        self.tracked_collision_objects_\
+                = self.tracking_config_["tracked_objects"]
+        # Change key to int
+        for k in self.tracked_collision_objects_.keys():
+            self.tracked_collision_objects_[int(k)]\
+                    = self.tracked_collision_objects_.pop(k)
+
+        rospy.Subscriber(
+                self.tracking_config_["phasespace_topic"],
+                PhaseSpaceMarkerArray,
+                self.phasespace_callback)
+        self.phasespace_marker_array = None
+
+        # Original origin is defined originally on the floor.
+        # New origin is the one we are using
+        # - x to the right, y to the front, z to the top
+        self.originalOrigin_2_newOrigin\
+                = np.array([[-1,0,0,0], [0,0,1,0], [0,1,0,0], [0,0,0,1]])
+
+    def phasespace_callback(self, data):
+        self.phasespace_marker_array = data
+
+    def publish_tf(self):
+        while not rospy.is_shutdown():
+            if self.phasespace_marker_array != None:
+                for ps_obj in self.phasespace_marker_array.data:
+                    k = int(ps_obj.id)
+                    if k in self.tracked_collision_objects_.keys():
+                        obj = self.tracked_collision_objects_[k]
+                        # trans = [ps_obj.point.x/1000., ps_obj.point.y/1000.,
+                                # ps_obj.point.z/1000.]
+                        # We exchange y and z so that it is more intuitive
+                        trans = [ps_obj.point.x/1000., ps_obj.point.y/1000.,
+                                ps_obj.point.z/1000.]
+                        quat = [0,0,0,1]
+                        originalOrigin_2_obj_mat\
+                                = tf.transformations.translation_matrix(trans)
+                        newOrigin_2_obj_mat = np.dot(
+                                np.linalg.inv(self.originalOrigin_2_newOrigin),
+                                originalOrigin_2_obj_mat)
+                        # self.tracked_collision_objects_[k][
+                                # "newOrigin_2_obj_mat"] = newOrigin_2_obj_mat
+                        self.br_.sendTransform(
+                                tf.transformations.translation_from_matrix(
+                                    newOrigin_2_obj_mat),
+                                tf.transformations.quaternion_from_matrix(
+                                    newOrigin_2_obj_mat),
+                                rospy.Time.now(),
+                                obj["frame_id"], obj["base_frame"])
+ 
+            self.publish_rate_.sleep()
+
+
+if __name__ == '__main__':
+    rospy.init_node('Msg2Tf', anonymous=True)
+    # Make sure sim time is working
+    while not rospy.Time.now():
+        pass
+    x = Msg2Tf()
+    x.publish_tf()
