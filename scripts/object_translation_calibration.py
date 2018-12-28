97e6e5543ceaf08e077c1a6e6df20a909e7a043c
Shen Li <shentheman.c@gmail.com>
2018-12-28 13:22:58 -0500

    add elbow as tracking

--- scripts/object_translation_calibration.py
+++ scripts/object_translation_calibration.py
@@ -10,7 +10,9 @@ import tf.transformations
 from phasespace_acquisition.msg import PhaseSpaceMarker, PhaseSpaceMarkerArray
 
 # "phasespace/palm/left"
-PHASESPACE_ID_FOR_CALIBRATION = 1
+# PHASESPACE_ID_FOR_CALIBRATION = 1
+# phasespace/albow
+PHASESPACE_ID_FOR_CALIBRATION = 8
 BASE_FRAME = "/odom_combined"
 
 class ObjectTranslationCalibration(object):
